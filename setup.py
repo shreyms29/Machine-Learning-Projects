a4f207f5afe60a97ed016f2f46dd1f233ebbbf78
ShreyasMS <shreyas.shre008@gmail.com>
2023-03-02 20:16:29 +0530

    Folders are created

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
@@ -6,13 +6,12 @@ PROJECT_NAME="shipment-pricing"
 VERSION="0.0.1"
 AUTHOR="Shreyas M S"
 DESRCIPTION="Predicting the Shipment pricing"
-PACKAGES=["Shipment_pricing"]
 REQUIREMENTS_FILE_NAME="requirements.txt"
 
 
 def get_requirements_list()->List[str]:
     with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readlines()
+        return requirements_file.readlines().remove("-e .")
 
 
 
@@ -21,7 +20,7 @@ name=PROJECT_NAME,
 version=VERSION,
 author=AUTHOR,
 description=DESRCIPTION,    
-packages=PACKAGES,
+packages=find_packages(),
 install_requires=get_requirements_list()
 
 )
