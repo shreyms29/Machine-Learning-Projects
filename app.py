fe649f9756f2634fb909bb94a0c1f53a7740d3c5
ShreyasMS <shreyas.shre008@gmail.com>
2023-03-03 00:19:36 +0530

    logging has been created

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from shipment.logger import logging
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return "Starting Machine Learning Projects"
+    logging.info("we are tseting logging")
+    return "CI CD pipeline has been established"
 
 if __name__=="__main__":
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ from typing import List
 
 
 PROJECT_NAME="shipment-pricing"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="Shreyas M S"
 DESRCIPTION="Predicting the Shipment pricing"
 REQUIREMENTS_FILE_NAME="requirements.txt"
 
 
+
 def get_requirements_list()->List[str]:
-    with open(REQUIREMENTS_FILE_NAME) as requirements_file:
-        return requirements_file.readlines().remove("-e .")
+    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
+        requirement_list = [lib_name.replace("\n","") for lib_name in requirement_file.readlines()]
+        print(requirement_list)
+        if "-e ." in requirement_list:
+            requirement_list.remove("-e .")
+        return requirement_list
 
 
 
@@ -22,6 +27,5 @@ author=AUTHOR,
 description=DESRCIPTION,    
 packages=find_packages(),
 install_requires=get_requirements_list()
-
 )
 
