23b4ca6f70b7debdb2d3387818fc54a5d35c0648
ShreyasMS <shreyas.shre008@gmail.com>
2023-03-03 14:34:52 +0530

    expection folder has been created

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+import os,sys
 from shipment.logger import logging
+from shipment.exception import ShipmentExpection
 
 app=Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    logging.info("we are tseting logging")
+    try:
+        raise Exception("we are testing")
+    except Exception as e:
+        shipment = ShipmentExpection(e,sys)
+        logging.info(shipment.error_message)
+        logging.info("we are testing logging")
     return "CI CD pipeline has been established"
 
 if __name__=="__main__":
